afa0037e5b28d442b8691a25563ee5a23be037ba
2fand <167662034+2fand@users.noreply.github.com>
2025-03-04 19:30:10 +0800

    Update stringPlus.h

--- stringPlus.h
+++ stringPlus.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <iostream>
+using namespace std;
+class stringPlus {
+private:
+	string last;//上次计算得到的结果
+public:
+	string getLast();//获取上次计算得到的结果
+	string calc(string str, string stra);//计算
+};

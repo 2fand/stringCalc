ad369340bcbe1ff7ed459d7412601aedfcbf278c
2fand <167662034+2fand@users.noreply.github.com>
2025-03-10 20:12:21 +0800

    Update stringPlus.cpp

--- stringPlus.cpp
+++ stringPlus.cpp
@@ -5,11 +5,17 @@ string stringPlus::getLast() {//获取上次计算得到的结果
 string stringPlus::calc(string str, string stra) {//计算
 	this->last.clear();
 	int addNum = 0;
+	int ix = 0;
+	int iy = 0;
 	bool itIsZero = !str.size();
 	bool itaIsZero = !stra.size();
 	bool strIsNegative = (str.size() ? '-' == str.front() : false);
 	bool straIsNegative = (stra.size() ? '-' == stra.front() : false);
-	bool isNegative = false;
+	bool tenSub = 1 == false;
+	bool isNegative = '-' == (abs(atoi(str.c_str())) > abs(atoi(stra.c_str())) ? str.front() : stra.front());
+	if (abs(atoi(stra.c_str())) > abs(atoi(str.c_str()))) {
+		str.swap(stra);
+	}
 	auto it = str.crbegin();
 	auto ita = stra.crbegin();
 	int digitNum = 0;
@@ -20,14 +26,14 @@ string stringPlus::calc(string str, string stra) {//计算
 		if (stra.crend() == ita || '0' > *ita || '9' < *ita) {
 			itaIsZero = true;
 		}
-		digitNum = (itIsZero ? 0 : (1 - strIsNegative * 2) * (*it - '0')) + (itaIsZero ? 0 : (1 - straIsNegative * 2) * (*ita - '0')) + addNum;
-		isNegative = digitNum ? digitNum < 0 : isNegative;
-		addNum = (1 - 2 * (itIsZero || str.crend() == it ? 0 : 1 - 2 * strIsNegative * *it) < (itaIsZero || stra.crend() == ita ? 0 : 1 - 2 * straIsNegative * *ita)) * digitNum / 10;
-		digitNum = (itIsZero || str.crend() == it ? 0 : 1 - 2 * strIsNegative * *it) < (itaIsZero || stra.crend() == ita ? 0 : 1 - 2 * straIsNegative * *ita) ? 10 + digitNum : abs(digitNum %= 10);
+		ix = (1 - 2 * strIsNegative) * (itIsZero ? 0 : *it++ - '0');
+		iy = (1 - 2 * straIsNegative) * (itaIsZero ? 0 : *ita++ - '0');
+		digitNum = ix + iy + !(0 > addNum && 2 == itIsZero + itaIsZero) * addNum;
+		addNum = abs(ix) < abs(iy) ? -1 : digitNum / 10;
+		digitNum = abs(digitNum % 10);
 		if (!itaIsZero || !itIsZero || addNum) {
 			this->last.insert(this->last.begin(), '0' + digitNum);
 		}
-		(itIsZero || (it++, 0)), (itaIsZero || (ita++, 0));
 	}
 	if (this->last.empty()) {
 		this->last.push_back('0');

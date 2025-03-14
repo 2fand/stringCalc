6292e948b77cc1bd3cd1d9316d33c0094f5273be
2fand <167662034+2fand@users.noreply.github.com>
2025-03-14 19:42:05 +0800

    Update stringPlus.cpp

--- stringPlus.cpp
+++ stringPlus.cpp
@@ -16,6 +16,14 @@ string stringPlus::calc(string str, string stra) {//计算
 	int id = 0;
 	int toDel = 0;
 	string tempstr;
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
 	if (abs(atoi(stra.c_str())) > abs(atoi(str.c_str()))) {
 		str.swap(stra);
 	}

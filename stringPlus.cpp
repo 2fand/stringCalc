d8021f57ea5333aaf0e686954e6c3998e6e3f098
2fand <167662034+2fand@users.noreply.github.com>
2025-03-19 20:35:08 +0800

    Update stringPlus.cpp

--- stringPlus.cpp
+++ stringPlus.cpp
@@ -46,7 +46,7 @@ string stringPlus::add(string str, string stra) {//加法计算
 		digitNum += !(0 > addNum && 2 == itIsZero + itaIsZero) * (1 - 2 * (digitNum < 0)) * addNum;
 		addNum = tenSub && abs(ix) < abs(iy) ? -1 : digitNum / 10;
 		digitNum = abs(digitNum % 10);
-		if (!itIsZero || !itaIsZero || addNum) {
+		if (!itIsZero || !itaIsZero || addNum || digitNum) {
 			this->last.insert(this->last.begin(), '0' + digitNum);
 		}
 	}

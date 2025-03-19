f1160f12b06c20f78a03a24a7983853513ddd460
2fand <167662034+2fand@users.noreply.github.com>
2025-03-19 20:36:02 +0800

    Add files via upload

---
+++ stringTim.cpp
@@ -0,0 +1,22 @@
+#include "stringTim.h"
+string stringTim::tim(string str, string stra) {
+	string timStr = "0";
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	bool isNegative = '-' == stra.front() && atoi(stra.c_str());
+	int i = abs(atoi(stra.c_str()));
+	for (; i; i--) {
+		timStr = add(timStr, str);
+	}
+	if (isNegative) {
+		timStr.insert(0, "-");
+	}
+	this->last = timStr;
+	return timStr;
+}
---
+++ stringTim.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stringPlus.h"
+class stringTim : public stringPlus {
+public:
+	string tim(string str, string stra);
+};
+

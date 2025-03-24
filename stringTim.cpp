b0255acc68c2c02f9aa2f5eef0bea90332adac42
2fand <167662034+2fand@users.noreply.github.com>
2025-03-24 19:46:32 +0800

    Update stringTim.cpp

--- stringTim.cpp
+++ stringTim.cpp
@@ -1,5 +1,5 @@
 #include "stringTim.h"
-string stringTim::tim(string str, string stra) {//乘法计算
+string stringTim::tim(string str, string stra) {//整数乘法计算
 	string timStr = "0";
 	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
 		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
@@ -24,5 +24,32 @@ string stringTim::tim(string str, string stra) {//乘法计算
 		}
 	}
 	this->last = timStr;
+	return timStr;
+}
+string stringTim::timAssign(string& str, string stra) {//整数乘等计算
+	string timStr = "0";
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	bool isNegative = 1 == ('-' == str.front()) + ('-' == stra.front()) && (atoi(str.c_str()) && atoi(stra.c_str()));
+	int i = abs(atoi(stra.c_str()));
+	for (; i; i--) {
+		timStr = add(timStr, str);
+	}
+	if (isNegative) {
+		timStr.insert(0, "-");
+		if ('-' == timStr[0] && '-' == timStr[1]) {
+			for (int i = 0; i < timStr.size() - 2; i++) {
+				timStr[i] = timStr[i + 2];
+			}
+			timStr.erase(timStr.size() - 2);
+		}
+	}
+	str = this->last = timStr;
 	return timStr;
 }

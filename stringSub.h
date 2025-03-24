5fed20766d3647eab7b8d629ab9e12833ea7e931
2fand <167662034+2fand@users.noreply.github.com>
2025-03-24 19:46:05 +0800

    Update stringSub.h

--- stringSub.h
+++ stringSub.h
@@ -4,8 +4,9 @@
 using namespace std;
 class stringSub{
 protected:
-	string last;//上次减法计算的结果
+	string last;//上次整数减法计算的结果
 public:
-	virtual string getLast();//获取上次减法计算的结果
-	virtual string sub(string str, string stra);//减法计算
+	virtual string getLast();//获取上次整数减法计算的结果
+	virtual string sub(string str, string stra);//整数减法计算
+	virtual string subAssign(string& str, string stra);//整数减等计算
 };

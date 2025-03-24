8a0acec1c35a465ae00c7fa20b8090c92ae9d530
2fand <167662034+2fand@users.noreply.github.com>
2025-03-24 19:44:29 +0800

    Update stringPlus.h

--- stringPlus.h
+++ stringPlus.h
@@ -3,8 +3,9 @@
 using namespace std;
 class stringPlus {
 private:
-	string last;//上次加法计算得到的结果
+	string last;//上次整数加法计算得到的结果
 public:
-	virtual string getLast();//获取上次加法计算得到的结果
-	virtual string add(string str, string stra);//加法计算
+	virtual string getLast();//获取上次整数加法计算得到的结果
+	virtual string add(string str, string stra);//整数加法计算
+	virtual string addAssign(string& str, string stra);//整数加等计算
 };

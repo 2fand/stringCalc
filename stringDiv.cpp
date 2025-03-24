84c5dc8f3c5cbee916f8635031ba32fc2539265f
2fand <167662034+2fand@users.noreply.github.com>
2025-03-24 19:42:08 +0800

    Update stringDiv.cpp

--- stringDiv.cpp
+++ stringDiv.cpp
@@ -22,3 +22,26 @@ string stringDiv::div(string str, string stra) {//整数除法计算
 	this->last = divStr;
 	return divStr;
 }
+string stringDiv::divAssign(string& str, string stra) {//整数除等计算
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	bool isNegative = (1 == ('-' == str.front()) + ('-' == stra.front()));
+	'-' == str.front() ? str = sub("0", str) : "";
+	'-' == stra.front() ? stra = sub("0", stra) : "";
+	string divStr = "0";
+	while (atoi(sub(str, stra).c_str()) >= 0) {
+		str = sub(str, stra);
+		divStr = sub(divStr, "-1");
+	}
+	if (isNegative) {
+		divStr.insert(0, "-");
+	}
+	str = this->last = divStr;
+	return divStr;
+}

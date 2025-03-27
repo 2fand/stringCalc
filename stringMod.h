718bc812fde2b601629ea97fd56119420ca5dad9
2fand <167662034+2fand@users.noreply.github.com>
2025-03-27 19:49:39 +0800

    Update stringMod.h

--- stringMod.h
+++ stringMod.h
@@ -2,6 +2,7 @@
 #include "stringSub.h"
 class stringMod : public stringSub {
 public:
+	string getLast();//获取上次模运算之后的结果
 	string mod(string str, string stra);//模计算
 	string modAssign(string& str, string stra);//模等计算
 };

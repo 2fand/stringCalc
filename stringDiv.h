9fcd3f63680438aba20afc6649997f6dc264453b
2fand <167662034+2fand@users.noreply.github.com>
2025-03-28 20:16:06 +0800

    Update stringDiv.h

--- stringDiv.h
+++ stringDiv.h
@@ -1,8 +1,10 @@
 #pragma once
 #include "stringSub.h"
 class stringDiv : protected stringSub {
+protected:
+	string last;//除法计算的结果
 public:
-	virtual string getLast();//获取上次除法计算出的结果
+	virtual string getLast();//获取上次除法计算的结果
 	virtual string div(string str, string stra);//整数除法计算
 	virtual string divAssign(string& str, string stra);//整数除等计算
 	virtual string div(string str, string stra, bool isUp);//整数除法向上/下取整计算

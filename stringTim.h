bfc1e97697e2c729e51a5d224d85c1e73a64fe40
2fand <167662034+2fand@users.noreply.github.com>
2025-03-28 20:15:33 +0800

    Update stringTim.h

--- stringTim.h
+++ stringTim.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "stringPlus.h"
-class stringTim : public stringPlus {
+class stringTim : protected stringPlus {
+protected:
+	string last;//上次乘法计算的结果
 public:
+	virtual string getLast();//获取上次乘法计算的结果
 	virtual string tim(string str, string stra);//整数乘法计算
 	virtual string timAssign(string& str, string stra);//整数乘等计算
 };

53dfba70ba83bb3604e36d477899acdd0743a8ca
2fand <167662034+2fand@users.noreply.github.com>
2025-03-29 17:43:59 +0800

    Update stringMod.cpp

--- stringMod.cpp
+++ stringMod.cpp
@@ -1,11 +1,11 @@
 #include "stringMod.h"
 string stringMod::mod(string str, string stra) {//模计算
 	this->last = sub(str, tim(stra, div(str, stra, false)));
-	return str;
+	return this->last;
 }
 string stringMod::modAssign(string& str, string stra) {//模等计算
 	this->last = subAssign(str, tim(stra, div(str, stra, false)));
-	return str;
+	return this->last;
 }
 string stringMod::getLast() {//获取上次模运算之后的结果
 	return this->last;

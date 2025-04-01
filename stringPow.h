aa58a96e16074599d67933473435258c812db0d9
2fand <167662034+2fand@users.noreply.github.com>
2025-04-01 19:08:12 +0800

    Update stringPow.h

--- stringPow.h
+++ stringPow.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "stringDiv.h"
 class stringPow : protected stringDiv {
-	string last;
+	string last;//上次次方计算后的值
 public:
-	string getLast();
-	string pow(string str, string stra);
-	string powAssign(string& str, string stra);
+	string getLast();//获取上次次方计算后的值
+	string pow(string str, string stra);//整数次方计算
+	string powAssign(string& str, string stra);//整数次等计算
 };

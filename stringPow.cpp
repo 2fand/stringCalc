4495f2071ce19ffb4ee2cda9bfb7984855d7a847
2fand <167662034+2fand@users.noreply.github.com>
2025-04-01 19:08:51 +0800

    Update stringPow.cpp

--- stringPow.cpp
+++ stringPow.cpp
@@ -1,10 +1,10 @@
 #include "stringPow.h"
-string stringPow::getLast() {
+string stringPow::getLast() {//获取上次次方计算后的值
 	return this->last;
 }
-string stringPow::pow(string str, string stra) {
+string stringPow::pow(string str, string stra) {//整数次方计算
 	return this->last;
 }
-string stringPow::powAssign(string& str, string stra) {
+string stringPow::powAssign(string& str, string stra) {//整数次等计算
 	return this->last;
 }

10fce5289bed329c3138e26898ae88f88a3e6243
2fand <167662034+2fand@users.noreply.github.com>
2025-04-01 19:09:16 +0800

    Update stringPow.h

--- stringPow.h
+++ stringPow.h
@@ -4,6 +4,6 @@ class stringPow : protected stringDiv {
 	string last;//上次次方计算后的值
 public:
 	string getLast();//获取上次次方计算后的值
-	string pow(string str, string stra);//整数次方计算
-	string powAssign(string& str, string stra);//整数次等计算
+	string pow(string str, string stra);//整数次方计算(未完工)
+	string powAssign(string& str, string stra);//整数次等计算(未完工)
 };

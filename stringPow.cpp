d88e1eb6a3c08e8563fe0da437abac2e46a5688e
2fand <167662034+2fand@users.noreply.github.com>
2025-04-02 20:24:00 +0800

    Update stringPow.cpp

--- stringPow.cpp
+++ stringPow.cpp
@@ -2,9 +2,59 @@
 string stringPow::getLast() {//获取上次次方计算后的值
 	return this->last;
 }
-string stringPow::pow(string str, string stra) {//整数次方计算(未完工)
-	return this->last;
+string stringPow::pow(string str, string stra) {//整数次方计算
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	int i = atoi(stra.c_str());
+	string powStr = i < 0 && '-' == str.front() ? "-1" : "1";
+	i < 0 && '-' == str.front() ? (str = sub("0", str)) : "";
+	for (; i; i < 0 ? i++ : i--) {
+		if (i < 0) {
+			divAssign(powStr, str);
+		}
+		else {
+			timAssign(powStr, str);
+		}
+	}
+	return this->last = powStr;
 }
-string stringPow::powAssign(string& str, string stra) {//整数次等计算(未完工)
-	return this->last;
+string stringPow::powAssign(string& str, string stra) {//整数次等计算
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && '-' != str[index] && ('0' > str[index] || '9' < str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && '-' != stra[index] && ('0' > stra[index] || '9' < stra[index])) {
+			stra.erase(index);
+		}
+	}
+	if (!str.size()) {
+		str = "0";
+	}
+	if (!stra.size()) {
+		stra = "0";
+	}
+	int i = atoi(stra.c_str());
+	string powStr = i < 0 && '-' == str.front() ? "-1" : "1";
+	i < 0 && '-' == str.front() ? (str = sub("0", str)) : "";
+	for (; i; i < 0 ? i++ : i--) {
+		if (i < 0) {
+			divAssign(powStr, str);
+		}
+		else {
+			timAssign(powStr, str);
+		}
+	}
+	return this->last = str = powStr;
 }

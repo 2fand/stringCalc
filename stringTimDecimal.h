bcf7937e63ddc14d0ae04d0ab760c3b0ac18b4ab
2fand <167662034+2fand@users.noreply.github.com>
2025-04-05 19:04:37 +0800

    Update stringTimDecimal.h

--- stringTimDecimal.h
+++ stringTimDecimal.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "stringTim.h"
-class stringTimDecimal : protected stringTim{
+class stringTimDecimal : protected stringTim{//小数乘法类
 	string last;
 public:
-	virtual string getLast();
-	virtual string timDecimal(string str, string stra);
-	virtual string timDecimalAssign(string& str, string stra);
+	virtual string getLast();//获取上次小数乘法计算的结果
+	virtual string timDecimal(string str, string stra);//小数乘法计算
+	virtual string timDecimalAssign(string& str, string stra);//小数乘等计算
 };
 

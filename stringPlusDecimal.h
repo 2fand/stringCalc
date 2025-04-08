9685e454867a7d009dd0f3d3b26e68c10aea4fdb
2fand <167662034+2fand@users.noreply.github.com>
2025-04-08 19:56:43 +0800

    Update stringPlusDecimal.h

--- stringPlusDecimal.h
+++ stringPlusDecimal.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "stringTimDecimal.h"
-class stringPlusDecimal : public stringTimDecimal {
+class stringPlusDecimal : public stringTimDecimal {//小数加法计算类
 protected:
-	string last;
+	string last;//上次小数加法计算之后的结果
 public:
-	virtual string getLast();
-	virtual string addDecimal(string str, string stra);
-	virtual string addDecimalAssign(string& str, string stra);
+	virtual string getLast();//获取上次小数加法计算之后的结果
+	virtual string addDecimal(string str, string stra);//小数加法计算
+	virtual string addDecimalAssign(string& str, string stra);//小数加等计算
 };
 

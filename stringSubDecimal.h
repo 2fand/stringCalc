98e2148b160d9544af5d7d79cba9a07f49f99428
2fand <167662034+2fand@users.noreply.github.com>
2025-04-12 17:54:44 +0800

    Update stringSubDecimal.h

--- stringSubDecimal.h
+++ stringSubDecimal.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "stringTimDecimal.h"
-class stringSubDecimal : protected stringTimDecimal {
+class stringSubDecimal : protected stringTimDecimal {//小数减法计算类
 protected:
-	string last;
+	string last;//上次小数减法计算之后的结果
 public:
-	virtual string getLast();
-	virtual string subDecimal(string str, string stra);
-	virtual string subDecimalAssign(string& str, string stra);
+	virtual string getLast();//获取上次小数减法计算之后的结果
+	virtual string subDecimal(string str, string stra);//小数减法计算
+	virtual string subDecimalAssign(string& str, string stra);//小数减等计算
 };
 

ce9905e3fd7d5538789e91f77aaec791f2bff047
2fand <167662034+2fand@users.noreply.github.com>
2025-04-15 19:52:01 +0800

    Update stringDivDecimal.cpp

--- stringDivDecimal.cpp
+++ stringDivDecimal.cpp
@@ -1,10 +1,10 @@
 #include "stringDivDecimal.h"
-string stringDivDecimal::getLast() {
+string stringDivDecimal::getLast() {//获取上次小数除法计算之后的结果
 	return this->last;
 }
-string stringDivDecimal::divDecimal(string str, string stra) {
+string stringDivDecimal::divDecimal(string str, string stra) {//小数除法计算
 
 }
-string stringDivDecimal::divDecimalAssign(string& str, string stra) {
+string stringDivDecimal::divDecimalAssign(string& str, string stra) {//小数除等计算
 	
 }

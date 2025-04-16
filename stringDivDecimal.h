9dfe3075db87ff848d774d0980ff69af88e88cc0
2fand <167662034+2fand@users.noreply.github.com>
2025-04-16 20:33:59 +0800

    Update stringDivDecimal.h

--- stringDivDecimal.h
+++ stringDivDecimal.h
@@ -3,9 +3,9 @@
 class stringDivDecimal : protected stringSubDecimal {
 protected:
 	string last;//上次小数除法计算之后的结果
+	void swap(char& ch, char& cha);//交换字符
 public:
 	string getLast();//获取上次小数除法计算之后的结果
 	string divDecimal(string str, string stra);//小数除法计算
 	string divDecimalAssign(string& str, string stra);//小数除等计算
 };
-

f133c7192c76a2acc8132db69652b2ea868cfb01
2fand <167662034+2fand@users.noreply.github.com>
2025-04-19 17:59:30 +0800

    Update stringPow.h

--- stringPow.h
+++ stringPow.h
@@ -1,10 +1,10 @@
 #pragma once
-#include "stringDiv.h"
-class stringPow : protected stringDiv {//整数次方计算类
+#include "stringDivDecimal.h"
+class stringPow : protected stringDivDecimal {//小数次方计算类
 protected:
 	string last;//上次次方计算后的值
 public:
 	string getLast();//获取上次次方计算后的值
-	string pow(string str, string stra);//整数次方计算(未完工)
-	string powAssign(string& str, string stra);//整数次等计算(未完工)
+	string pow(string str, string stra);//小数次方计算
+	string powAssign(string& str, string stra);//小数次等计算(未完工)
 };

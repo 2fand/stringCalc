89abd20037d947649b7f2bd8330787a256a8bc35
2fand <167662034+2fand@users.noreply.github.com>
2025-04-19 17:59:03 +0800

    Update stringPow.cpp

--- stringPow.cpp
+++ stringPow.cpp
@@ -2,15 +2,15 @@
 string stringPow::getLast() {//获取上次次方计算后的值
 	return this->last;
 }
-string stringPow::pow(string str, string stra) {//整数次方计算
-        for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
-                if (index < str.size() && ('-' != str[index] && ('0' > str[index] || '9' < str[index]) || index > 0 && '-' == str[index])) {
-                        str.erase(index);
-                }
-                if (index < stra.size() && ('-' != stra[index] && ('1' > stra[index] || '9' < stra[index]) || index > 0 && '-' == stra[index])) {
-                        stra.erase(index);
-                }
-        }
+string stringPow::pow(string str, string stra) {//小数次方计算
+	for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
+		if (index < str.size() && ('-' != str[index] && ('0' > str[index] || '9' < str[index]) || index > 0 && '-' == str[index])) {
+			str.erase(index);
+		}
+		if (index < stra.size() && ('-' != stra[index] && ('0' > stra[index] || '9' < stra[index]) || index > 0 && '-' == stra[index])) {
+			stra.erase(index);
+		}
+	}
 	if (!str.size()) {
 		str = "0";
 	}
@@ -22,15 +22,15 @@ string stringPow::pow(string str, string stra) {//整数次方计算
 	i < 0 && '-' == str.front() ? (str = sub("0", str)) : "";
 	for (; i; i < 0 ? i++ : i--) {
 		if (i < 0) {
-			divAssign(powStr, str);
+			divDecimalAssign(powStr, str);
 		}
 		else {
-			timAssign(powStr, str);
+			timDecimalAssign(powStr, str);
 		}
 	}
 	return this->last = powStr;
 }
-string stringPow::powAssign(string& str, string stra) {//整数次等计算
+string stringPow::powAssign(string& str, string stra) {//小数次等计算
         for (int index = 0; index < (str.size() > stra.size() ? str.size() : stra.size()); index++) {
                 if (index < str.size() && ('-' != str[index] && ('0' > str[index] || '9' < str[index]) || index > 0 && '-' == str[index])) {
                         str.erase(index);
@@ -50,10 +50,10 @@ string stringPow::powAssign(string& str, string stra) {//整数次等计算
 	i < 0 && '-' == str.front() ? (str = sub("0", str)) : "";
 	for (; i; i < 0 ? i++ : i--) {
 		if (i < 0) {
-			divAssign(powStr, str);
+			divDecimalAssign(powStr, str);
 		}
 		else {
-			timAssign(powStr, str);
+			timDecimalAssign(powStr, str);
 		}
 	}
 	return this->last = str = powStr;

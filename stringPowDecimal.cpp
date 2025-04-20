e6711dda7d4b944a8cf9ff96596e6d9507d32dce
2fand <167662034+2fand@users.noreply.github.com>
2025-04-20 17:29:35 +0800

    Update stringPowDecimal.cpp

--- stringPowDecimal.cpp
+++ stringPowDecimal.cpp
@@ -1,14 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS true
 #include "stringPowDecimal.h"
-string stringPowDecimal::getLast() {
+string stringPowDecimal::getLast() {//获取上次指数为小数的次方计算之后的结果
 	return this->last;
 }
-string stringPowDecimal::powDecimal(string str, string stra) {
+string stringPowDecimal::powDecimal(string str, string stra) {//指数为小数的次方计算
 	char s[10000] = "";
 	sprintf(s, "%lf", pow(strtod(str.c_str(), nullptr), strtod(stra.c_str(), nullptr)));
 	regex r("\\.?0+$");
 	return regex_replace(string(s), r, "");
 }
-string stringPowDecimal::powDecimalAssign(string& str, string stra) {
+string stringPowDecimal::powDecimalAssign(string& str, string stra) {//指数为小数的次等计算
 	return this->last = str = powDecimal(str, stra);
 }

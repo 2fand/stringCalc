9b9568b1e8b400381abdf26b3b35f49fb91ebf5f
2fand <167662034+2fand@users.noreply.github.com>
2025-04-20 17:30:09 +0800

    Update stringPowDecimal.h

--- stringPowDecimal.h
+++ stringPowDecimal.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "stringDivDecimal.h"
-class stringPowDecimal : protected stringDivDecimal {//指数为小鼠的次方计算类
-	string last;//上次指数为小鼠的次方计算之后的结果
+class stringPowDecimal : protected stringDivDecimal {//指数为小数的次方计算类
+	string last;//上次指数为小数的次方计算之后的结果
 public:
-	string getLast();//获取上次指数为小鼠的次方计算之后的结果
-	string powDecimal(string str, string stra);//指数为小鼠的次方计算
-	string powDecimalAssign(string& str, string stra);//指数为小鼠的次等计算
+	string getLast();//获取上次指数为小数的次方计算之后的结果
+	string powDecimal(string str, string stra);//指数为小数的次方计算
+	string powDecimalAssign(string& str, string stra);//指数为小数的次等计算
 };
